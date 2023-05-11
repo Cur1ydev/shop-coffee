650da3a78b330af7658a79dccd6741b8624edcf6
Phạm Lợi <loixoan28.07@gmail.com>
2023-05-11 10:46:18 +0700

    lợi xoăn commit

---
+++ database/migrations/2023_05_10_233827_create_product_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->integer('category_id');
+            $table->string('name');
+            $table->string('slug');
+            $table->string('image');
+            $table->integer('price');
+            $table->integer('discount');
+            $table->tinyInteger('status');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product');
+    }
+};
---
+++ database/migrations/2023_05_11_001802_create_orderitem_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orderitem', function (Blueprint $table) {
+            $table->id();
+            $table->integer('product_id');
+            $table->integer('attribute_id');
+            $table->integer('quantity');
+            $table->integer('total_price');
+            $table->integer('shipping_fee');
+            $table->string('order_code');
+            $table->string('coupon_code');
+            $table->string('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orderitem');
+    }
+};
